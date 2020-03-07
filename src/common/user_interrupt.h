3f30b62bab333c1c43a2aed21134aa66f7281d0e
Shlomi Nissan <admin@betamark.com>
2020-03-06 19:40:56 -0800

    Start a simple http server

---
+++ src/common/user_interrupt.h
@@ -0,0 +1,32 @@
+#ifndef USER_INTERRUPT_H_ 
+#define USER_INTERRUPT_H_
+
+#include <condition_variable>
+#include <mutex>
+#include <iostream>
+#include <csignal>
+
+static std::condition_variable condition;
+static std::mutex mutex;
+
+class UserInterrupt {
+public:
+    static void Register() {
+        signal(SIGINT, Handle);
+    }
+
+    static void Handle(int signal){
+        if (signal == SIGINT) {
+            condition.notify_one();
+        }
+    }
+
+    static void Wait() {
+        std::unique_lock<std::mutex> lock { mutex };
+        condition.wait(lock);
+        std::cout << "The service was interrupted" << "\n";
+        lock.unlock();
+    }
+};
+
+#endif  // USER_INTERRUPT_H_
--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,36 @@
 #include <iostream>
-#n
+#include <string>
+
+#include <cpprest/http_listener.h>
+
+#include "common/user_interrupt.h"
+#include "gflags/gflags.h"
+
+using namespace web::http::experimental::listener;
+
+DEFINE_string(port, "8080", "server port");
+DEFINE_string(host, "http://localhost", "server address");
+
 int main(int argc, char* argv[]) {
-    std::cout << "Hello world!\n";
+    gflags::SetUsageMessage("rest -port <PORT> -host <HOST>");
+    gflags::SetVersionString("1.0.0");
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+    UserInterrupt::Register();
+
+    std::string host = FLAGS_host + ":" + FLAGS_port;
+
+    auto listener = http_listener(host);
+
+    // start http server
+    listener.open().wait();
+    std::cout << "Listening for HTTP requests at " << host << '\n';
+    
+    // wait for user interrupt
+    UserInterrupt::Wait();
+    
+    // shutdown http server
+    listener.close().wait();
+
     return 0;
 }
